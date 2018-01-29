8c638857b5bacfed8d4c3fd5afcee65b9685e3cf
KatharinaSack <katharina.sack@ccca.ac.at>
2018-01-29 14:52:36 +0100

    adapted group_list_authz to list also groups with addition_without_group_membership True

--- ckanext/iapi/logic/action.py
+++ ckanext/iapi/logic/action.py
@@ -8,6 +8,8 @@ from ckan.logic import side_effect_free
 import os
 import hashlib
 import ckan.lib.uploader as uploader
+import ckan.authz as authz
+import ckan.lib.dictization.model_dictize as model_dictize
 
 ValidationError = ckan.logic.ValidationError
 NotFound = ckan.logic.NotFound
@@ -22,7 +24,7 @@ from ckanext.resourceversions import helpers as hr
 
 
 def package_show (context, data_dict):
-    
+
     # id kann key oder name sein; name braucht -vxx
     id = data_dict['id']
     r1 = re.compile("-v..$")
@@ -79,7 +81,10 @@ def resource_change_package(context, data_dict):
     resource_dict['package_id'] = pkg_dict_new['id']
     pkg_dict_new['resources'].append(resource_dict)
 
-    _get_action('package_update')(context, pkg_dict_new)
+    try:
+        _get_action('package_update')(context, pkg_dict_new)
+    except Exception, e:
+        print(e)
 
     return "package '" + package_id_new + "' contains now " + str(len(pkg_dict_new['resources'])) + " resource(s)"
 
@@ -119,3 +124,72 @@ def resource_get_hash(context, data_dict):
         return hasher.hexdigest()
     else:
         raise ValidationError({'order':'This is not an uploaded file'})
+
+
+def group_list_authz(context, data_dict):
+    '''Return the list of groups that the user is authorized to edit.
+
+    :param available_only: remove the existing groups in the package
+      (optional, default: ``False``)
+    :type available_only: boolean
+
+    :param am_member: if ``True`` return only the groups the logged-in user is
+      a member of, otherwise return all groups that the user is authorized to
+      edit (for example, sysadmin users are authorized to edit all groups)
+      (optional, default: ``False``)
+    :type am-member: boolean
+
+    :returns: list of dictized groups that the user is authorized to edit
+    :rtype: list of dicts
+
+    '''
+    model = context['model']
+    user = context['user']
+    available_only = data_dict.get('available_only', False)
+    am_member = data_dict.get('am_member', False)
+
+    _check_access('group_list_authz', context, data_dict)
+
+    sysadmin = authz.is_sysadmin(user)
+    roles = authz.get_roles_with_permission('manage_group')
+    if not roles:
+        return []
+    user_id = authz.get_user_id_for_username(user, allow_none=True)
+    if not user_id:
+        return []
+
+    if not sysadmin or am_member:
+        q = model.Session.query(model.Member) \
+            .filter(model.Member.table_name == 'user') \
+            .filter(model.Member.capacity.in_(roles)) \
+            .filter(model.Member.table_id == user_id) \
+            .filter(model.Member.state == 'active')
+        group_ids = []
+        for row in q.all():
+            group_ids.append(row.group_id)
+
+    q = model.Session.query(model.Group) \
+        .filter(model.Group.is_organization == False) \
+        .filter(model.Group.state == 'active')
+
+    groups = []
+
+    for row in q.all():
+        # packages can be added to groups with addition_without_group_membership True in any case
+        if row._extras.get('addition_without_group_membership', None) is not None and row._extras['addition_without_group_membership'].value == "True":
+            groups.append(row)
+
+    if not sysadmin or am_member:
+        q = q.filter(model.Group.id.in_(group_ids))
+
+    for row in q.all():
+        if row not in groups:
+            groups.append(row)
+
+    if available_only:
+        package = context.get('package')
+        if package:
+            groups = set(groups) - set(package.get_groups())
+
+    group_list = model_dictize.group_list_dictize(groups, context)
+    return group_list
